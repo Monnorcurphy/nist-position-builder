5f4fbebbf95a568da7ceb81e83e14bbed852df67
Connor Murphy <ConnorLMurphy@gmail.com>
2020-11-01 13:31:03 -0500

    Now able to put all roles in a csv

--- src/WorkRoles/workRole.js
+++ src/WorkRoles/workRole.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
-import { get } from "../shared-functions";
+import { capitalize, get } from "../shared-functions";
 import "./role-styles";
+import { CSVLink, CSVDownload } from "react-csv";
 
 const WorkRole = ({ id }) => {
   const [workRole, getWorkRole] = useState({});
@@ -16,9 +17,39 @@ const WorkRole = ({ id }) => {
     get(`/api/workrole/${identity}`, getWorkRole);
   }, [id, location.pathname]);
 
+  let headers = [
+    { label: "Work Role Id", key: "roleId" },
+    { label: "Work Role Title", key: "roleTitle" },
+    { label: "Work Role Description", key: "roleDescription" },
+    { label: "Statement Id", key: "statementId" },
+    { label: "Statement", key: "statementText" },
+    { label: "Statement Type", key: "statementType" },
+  ];
+
+  let data = [
+    { roleId: `${workRole.id}`, roleTitle: `${workRole.title}` },
+    { roleDescription: `${workRole.description}` },
+  ];
+  if (workRole.statements && workRole.statements.length) {
+    data = data.concat(
+      workRole.statements.map((statement) => {
+        let keys = Object.keys(statement);
+
+        let obj = {};
+        keys.forEach((key) => {
+          obj[`statement${capitalize(key)}`] = statement[`${key}`];
+        });
+        return obj;
+      })
+    );
+  }
+
   return (
     <div className="App">
       <header className="App-header">
+        <CSVLink headers={headers} data={data}>
+          Download this workrole
+        </CSVLink>
         <div>{workRole.id}</div>
         <div>{workRole.title}</div>
         <div>{workRole.description}</div>
